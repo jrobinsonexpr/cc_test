5de753a6a100baf2e3a64e725ea3b1fff5f79ee9
jrobinsonexpr <jrobinson@expression.net>
2017-02-28 12:02:49 -0500

    test

---
+++ testfile.js
@@ -0,0 +1,18 @@
+var foo = "hello world";
+var bar = "asdf";
+var good_obj = {
+	one: 1,
+	two: 2,
+	three: 3
+};
+var bad_obj = {
+	one:,
+	two:,
+	three
+}
+
+var good_test = "hello " + "world";
+console.log("good_test = ",good_test);
+
+var bad_test = "hello + " world;
+console.log("bad_test = ",bad_test);
